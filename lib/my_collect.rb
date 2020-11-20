22bf63d427ec97d2ecda50e2c62480c9cbf4d4bb
Blaire <grant.blaire@gmail.com>
2020-11-20 14:33:36 -0600

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
-
+def my_collect(array)
+    if block_given?
+        i = 0
+        collection = []
+        while i < array.length
+        collection << yield(array[i])
+        i += 1
+    end
+        collection
+    else
+        i = 0 
+    end
+  end
 
